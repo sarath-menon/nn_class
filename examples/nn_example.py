4dacd8076fa2ea22d8597c785b4ca8a9dd711fb9
Sarath S Menon <sarathmenon.downey@gmail.com>
2018-08-16 21:24:32 +0530

    First commit

---
+++ examples/nn_example.py
@@ -0,0 +1,17 @@
+# Dataset taken from Andrew NG's Deep learning course on coursera
+
+from nn_class_version_1 import nn
+from helper_functions import *
+from dnn_app_utils_v2 import load_data
+
+train_x_orig, Y_train, test_x_orig, test_y, classes = load_data()
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0],-1).T
+X_train= train_x_flatten/255.
+
+
+net = nn(X_train ,Y_train ,Plot_loss=True)
+net.create_layer('Input',Relu)
+net.create_layer(10 ,sigmoid)
+net.create_model()
+
+net.train()
