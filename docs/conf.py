ef9b90d19a66fa9c45865c365a95cb65e3d1f27e
Sarath S Menon <sarathmenon.downey@gmail.com>
2018-08-18 19:16:12 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,10 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_theme_options = {
+    'logo_only': True
+}
+
 html_theme = "sphinx_rtd_theme" 
 html_logo = "images/icon.png"
 
