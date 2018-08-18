7578d6b951e749135e3b5e800c81da4e093f80d4
Sarath S Menon <sarathmenon.downey@gmail.com>
2018-08-18 19:05:46 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme" 
-html_logo = "images/icon.svg"
+html_logo = "images/icon.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
